2d588691f644562deacd1212cb20c05b68440eca
Isabella Wloch <isabella.wloch@atomicobject.com>
2025-11-22 11:50:18 -0500

    added source words and started with get single word query

--- infinite-wordle/convex/words.ts
+++ infinite-wordle/convex/words.ts
@@ -1,4 +1,5 @@
 import { query } from "./_generated/server";
+import {v} from "convex/values";
 
 export const get = query({
     args: {},
@@ -6,3 +7,11 @@ export const get = query({
         return await ctx.db.query("words").collect();
     },
 });
+
+export const getWord = query({
+    args: {number: v.number()},
+    handler: async (ctx) => {
+        return (await ctx.db.query("words").filter(q => !q.field("played")).take(1).
+        collect()).length;
+    },
+})
