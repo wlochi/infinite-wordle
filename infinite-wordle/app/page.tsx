472b4e8ec3ba2337c774168c74a82fbc33b10bce
Isabella Wloch <isabella.wloch@atomicobject.com>
2025-11-22 14:40:33 -0500

    started doing the outline of the css for the wordle board

--- infinite-wordle/app/page.tsx
+++ infinite-wordle/app/page.tsx
@@ -1,15 +1,21 @@
 "use client";
-
-import Image from "next/image";
 import {useQuery} from "convex/react";
 import {api} from "@/convex/_generated/api";
 
 export default function Home() {
-    const words = useQuery(api.words.get);
-    console.log(words);
+    const word = useQuery(api.words.getWord);
+    console.log(word);
   return (
     <main className="flex min-h-screen flex-col p-24">
-        {words?.map(({ _id, word }) => <div key={_id}>{word}</div>)}
+        <div className="wordle-table">
+            <div className="wordle-row">
+                <input type="text" minLength={1} maxLength={1} className="word-box"/>
+                <input type="text" minLength={1} maxLength={1} className="word-box"/>
+                <input type="text" minLength={1} maxLength={1} className="word-box"/>
+                <input type="text" minLength={1} maxLength={1} className="word-box"/>
+                <input type="text" minLength={1} maxLength={1} className="word-box" />
+            </div>
+        </div>
     </main>
   );
 }
